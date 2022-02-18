cec83de2339dfb2bd193370cc489982fbc1a9c85
florian on nixos (Florian Brandes) <florian.brandes@posteo.de>
2022-02-18 10:58:52 +0100

    further improvements to flake.nix
    Signed-off-by: florian on nixos (Florian Brandes) <florian.brandes@posteo.de>

--- flake.nix
+++ flake.nix
@@ -13,45 +13,69 @@
 
         python = pkgs.python39;
         projectDir = ./.;
-        overrides = pkgs.poetry2nix.overrides.withDefaults (self: super: {
-          threadpoolctl = super.threadpoolctl.overrideAttrs (old: {
-            format = "flit";
-          });
-        });
-
+        overrides = [
+          pkgs.poetry2nix.defaultPoetryOverrides
+          (self: super: {
+            pikepdf = super.pikepdf.overrideAttrs (old: {
+              nativeBuildInputs = old.nativeBuildInputs ++ [ self.setuptools-scm-git-archive self.pybind11 ];
+              buildInputs = old.buildInputs ++ [ self.pybind11 self.setuptools-scm-git-archive pkgs.qpdf ];
+            });
+          })
+          (self: super: {
+            threadpoolctl = super.threadpoolctl.overrideAttrs (old: {
+              format = "other";
+              buildPhase = ''
+                runHook preBuild
+                ${python}/bin/python -m flit build --format wheel
+                runHook postBuild
+              '';
+              installPhase = ''
+                ${self.pip}/bin/pip install dist/*.whl --prefix $out
+              '';
+              nativeBuildInputs = [ self.flit self.flit-core self.wrapPython ];
+              #buildInputs = [ self.flit ];
+            });
+          })
+          (self: super: {
+            ocrmypdf = super.ocrmypdf.overrideAttrs (old: {
+              nativeBuildInputs = old.nativeBuildInputs ++ [ self.setuptools-scm-git-archive ];
+            });
+          })
+        ];
         packageName = "paperless-ngx";
+        path = pkgs.lib.makeBinPath [
+          pkgs.ghostscript
+          pkgs.imagemagick
+          pkgs.jbig2enc
+          pkgs.optipng
+          pkgs.pngquant
+          pkgs.qpdf
+          pkgs.tesseract4
+          pkgs.unpaper
+        ];
+
       in
       {
 
         packages.${packageName} = pkgs.poetry2nix.mkPoetryApplication {
           inherit python projectDir;
-          # Non-Python runtime dependencies go here
-          overrides = [
-            pkgs.poetry2nix.defaultPoetryOverrides
-            (self: super: {
-              pikepdf = super.pikepdf.overrideAttrs (old: {
-                nativeBuildInputs = old.nativeBuildInputs ++ [ self.setuptools-scm-git-archive self.pybind11 ];
-                buildInputs = old.buildInputs ++ [ self.pybind11 self.setuptools-scm-git-archive pkgs.qpdf ];
-              });
-            })
-            (self: super: {
-              threadpoolctl = super.threadpoolctl.overrideAttrs (old: {
-                format = "other";
-                buildPhase = ''
-                  runHook preBuild
-                  ${python}/bin/python -m flit build --format wheel
-                  runHook postBuild
-                '';
-                installPhase = ''
-                  ${self.pip}/bin/pip install dist/*.whl --prefix $out
-                '';
-                nativeBuildInputs = [ self.flit self.flit-core self.wrapPython ];
-                #buildInputs = [ self.flit ];
-              });
-            })
-          ];
-          propogatedBuildInputs = [ pkgs.python3Packages.setuptools-scm-git-archive ];
-          buildInputs = [ pkgs.python3Packages.pybind11 ];
+          overrides = overrides;
+          format = "other";
+          postPatch = ''
+            substituteInPlace gunicorn.conf.py --replace "bind = '0.0.0.0:8000'" ""
+          '';
+          buildPhase = ''
+            true
+          '';
+          installPhase = ''
+            mkdir -p $out/lib
+            cp -r . $out/lib/paperless-ng
+            chmod +x $out/lib/paperless-ng/src/manage.py
+            makeWrapper $out/lib/paperless-ng/src/manage.py $out/bin/paperless-ng \
+            --prefix PYTHONPATH : "$PYTHONPATH" \
+            --prefix PATH : "${path}"
+          '';
+
         };
 
         defaultPackage = self.packages.${system}.${packageName};
@@ -61,6 +85,8 @@
             buildInputs = [
               (pkgs.poetry2nix.mkPoetryEnv {
                 inherit python projectDir overrides;
+                #editablePackageSources = { paperless-ngx = "./src/"; };
+                extraPackages = ps: [ pkgs.imagemagick ];
               })
               pkgs.python39Packages.poetry
             ];
